036253c7270bb9fd3b38f6e99817301473daa5db
Nelson Gonzalez <nelglezfl@gmail.com>
2019-02-28 15:49:57 -0500

    Saving image to photo library

--- AstronomyUITests/AstronomyUITests.swift
+++ AstronomyUITests/AstronomyUITests.swift
@@ -10,6 +10,18 @@ import XCTest
 
 class AstronomyUITests: XCTestCase {
     
+    private var savePhotoButton: XCUIElement {
+        return app.buttons["PhotoDetailViewController.SaveButton"]
+    }
+    
+    private var statusLabel: XCUIElement {
+        return app.staticTexts["PhotoDetailViewController.CameraLabel"]
+    }
+    
+    private var saveButton: XCUIElement {
+        return app.buttons["PhotoDetailViewController.SaveButton"]
+    }
+    
     var app: XCUIApplication!
 
     override func setUp() {
@@ -19,7 +31,37 @@ class AstronomyUITests: XCTestCase {
         app = XCUIApplication()
 
         app.launch()
+
+        app.launchArguments.append("UITesting")
+    }
     
+    
+    func testSavingImageToPhotoLibrary() {
+        
+        
+        app.collectionViews.children(matching: .cell).element(boundBy: 0).otherElements.containing(.image, identifier:"MarsPlaceholder").element.tap()
+        app.otherElements.containing(.navigationBar, identifier:"Title").children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.tap()
+        app.staticTexts["Taken by 5 on 8/6/12, 8:00 PM (Sol 1)"].tap()
+        
+        
+        XCTAssertEqual("Mast Camera", statusLabel.label)
+      //  app.staticTexts["Mast Camera"].tap()
+        
+        saveButton.tap()
+      //  app/*@START_MENU_TOKEN@*/.buttons["PhotoDetailViewController.SaveButton"]/*[[".buttons[\"Save to Photo Library\"]",".buttons[\"PhotoDetailViewController.SaveButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        
+        app.alerts["“Astronomy” Would Like to Access Your Photos"]
+      //  let astronomyWouldLikeToAccessYourPhotosAlert = app.alerts["“Astronomy” Would Like to Access Your Photos"]
+        
+        
+        XCTAssertEqual("Save to Photo Library", saveButton.label)
+    
+        
+      //  astronomyWouldLikeToAccessYourPhotosAlert.staticTexts["“Astronomy” Would Like to Access Your Photos"].tap()
+        app.buttons["Okay"].tap()
+        
+        
     }
 
    
